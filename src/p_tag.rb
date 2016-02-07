e4a91dc3983f505f0b379259e7f9d3f3aff6e617
Peter Fry <peter@ourstage.com>
2016-02-07 13:23:33 -0500

    Fix bad case/when syntax.

--- src/p_tag.rb
+++ src/p_tag.rb
@@ -18,66 +18,67 @@ class PTag
 
   def self.body_replace(type, inner_text, section_index)
     return "<p>#{inner_text}</p>" if type == 'bib'
-    case type
-    when 'bl1_s', 'bl1_m', 'bl1_e'
+    case
+    when ['bl1_s', 'bl1_m', 'bl1_e'].include?(type)
       "<break/><list type=\"4\"><item><p align=\"left\">#{inner_text}</p></item></list><break/>"
-    when 'cap1', 'caption', 'v_s', 'v_m', 'v_e'
+    when ['cap1', 'caption', 'v_s', 'v_m', 'v_e'].include?(type)
       "<bq><p align=\"left\"><emph type=\"2\">#{inner_text}</emph></p></bq>"
-    when 'dlg1_s', 'dlg1_m', 'dlg1_e', 'ep1', 'index2', 'index3', 'v', 'v_epigraph'
+    when ['dlg1_s', 'dlg1_m', 'dlg1_e', 'ep1', 'index2', 'index3', 'v', 'v_epigraph'].include?(type)
       "<bq><p align=\"left\">#{inner_text}</p></bq>"
-    when 'ep1s_2'
+    when 'ep1s_2' == type
       "<bq><p align=\"center\"><emph type=\"2\">#{inner_text}</emph></p></bq>"
-    when 'ep1_2'
+    when 'ep1_2' == type
       "<bq><p align=\"center\">#{inner_text}</p></bq>"
-    when 'ext2'
+    when 'ext2' == type
       "<bq><p><bq><p>#{inner_text}</p></bq></p></bq>"
-    # when 'figureholder'
+    # when 'figureholder' == type
     #   "<fig id=\"???\" scale=\"1\"/>"
-    when 'fnottxt', 'footnotetext'
+    when ['fnottxt', 'footnotetext'].include?(type)
       "<p><footnote id=\"fn_#{section_index}_1\"><p>#{inner_text}</p></footnote></p>"
-    when 'heading1run-in'
+    when 'heading1run-in' == type
       "<p align=\"center\"><emph type=\"1\"><emph type=\"6\">#{inner_text}</emph></emph></p>"
-    when 'headingb'
+    when 'headingb' == type
       "<p align=\"left\"><emph type=\"2\">#{inner_text}</emph></p>"
-    when 'headingc'
+    when 'headingc' == type
       "<p align=\"left\"><emph type=\"6\">#{inner_text}</emph></p>"
-    when 'ep1s', 'ep1s_prologue', 'ep1schapteropener'
+    when ['ep1s', 'ep1s_prologue', 'ep1schapteropener'].include?(type)
       "<bq><p align=\"right\">#{inner_text}</p></bq>"
-    when 'heading1', 'heading1withrun-in', 'headingnotes'
+    when ['heading1', 'heading1withrun-in', 'headingnotes'].include?(type)
       "<p align=\"center\"><emph type=\"6\">#{inner_text}</emph></p>"
-    when 'heading2', 'heading3', 'hn', 'pt_txt'
+    when ['heading2', 'heading3', 'hn', 'pt_txt'].include?(type)
       "<p align=\"center\"><emph type=\"2\">#{inner_text}</emph></p>"
-    when 'nl1_s', 'nl1_m', 'nl1_e'
+    when ['nl1_s', 'nl1_m', 'nl1_e'].include?(type)
       "<break/><list type=\"1\"><item><p align=\"left\">#{inner_text}</p></item></list><break/>"
-    when 'ul1_s', 'ul1_m', 'ul1_e'
+    when ['ul1_s', 'ul1_m', 'ul1_e'].include?(type)
       "<break/><list type=\"6\"><item><p align=\"left\">#{inner_text}</p></item></list><break/>"
-    when 'headinga', 'tablehead', 'vparthead'
+    when ['headinga', 'tablehead', 'vparthead'].include?(type)
       "<p align=\"left\"><emph type=\"1\">#{inner_text}</emph></p>"
-    when 'GL'
+    when 'GL' == type
       "<break/><p align=\"left\">#{inner_text}</p><break/>"
-    when 'ext1_s', 'ext1s'
+    when ['ext1_s', 'ext1s'].include?(type)
       "<bq><p align=\"left\">#{inner_text}</p></bq>"
-    when 'ext1_s+bold', 'ext1_m+bold', 'ext1_e+bold'
+    when ['ext1_s+bold', 'ext1_m+bold', 'ext1_e+bold'].include?(type)
       "<bq><p align=\"left\"><emph type=\"1\">#{inner_text}</emph></p></bq>"
-    when /enottxt\(cont/.match(type), /ext1/.match(type)
+    when /enottxt\(cont/.match(type) || /ext1/.match(type)
       "<bq><p>#{inner_text}</p></bq"
-    when 'annotationtext', 'ack', 'body(first)', 'bodyfirst', 'indexalpha', 'index1', 'normal', 'placehld',
-         'tb_fl', 'th_fl', 'txacknowledgments', /xpara/.match(type), /copyright/.match(type), /enottxt/.match(type),
-         /tabletext/.match(type)
+    when ['annotationtext', 'ack', 'body(first)', 'bodyfirst', 'indexalpha', 'index1', 'normal', 'placehld',
+         'tb_fl', 'th_fl', 'txacknowledgments'].include?(type)
       "<p align=\"left\">#{inner_text}</p>"
-    when 'ca', 'letterauthor'
+    when /xpara/.match(type) || /copyright/.match(type) || /enottxt/.match(type) || /tabletext/.match(type)
+      "<p align=\"left\">#{inner_text}</p>"
+    when ['ca', 'letterauthor'].include?(type)
       "<p align=\"right\">#{inner_text}</p>"
-    when 'diaryorn', 'th_ctr'
+    when ['diaryorn', 'th_ctr'].include?(type)
       "<p align=\"center\">#{inner_text}</p>"
-    when 'cst', '-et'
+    when ['cst', '-et'].include?(type)
       "<subtitle>#{inner_text}</subtitle>"
-    when 'cn', 'ct', '-ct', 'en', 'et', 'extt', 'pt'
+    when ['cn', 'ct', '-ct', 'en', 'et', 'extt', 'pt'].include?(type)
       "<title>#{inner_text}</title>"
     when /spcbrk2/.match(type)
       '<break type="graphic"/>'
-    when '1/2ls'
+    when '1/2ls' == type
       "<break type=\"space\"/><bq><p>#{inner_text}</p></bq>"
-    when '1ls', /spcbrk/.match(type)
+    when '1ls' == type || /spcbrk/.match(type)
       '<break type="space"/>'
     end
   end
--- src/style_tag.rb
+++ src/style_tag.rb
@@ -3,37 +3,35 @@ class StyleTag
     type = node.get_attribute('type').downcase.gsub(' ', '')
     return '<p align="center"/>' if type == 'strikethrough'
     inner_text = node.inner_html.strip
-    replace = case type
-              when 'bold|italic|smallcaps'
+    replace = case
+              when type == 'bold|italic|smallcaps'
                 "<emph type=\"10\"><emph type=\"6\">#{inner_text}</emph></emph>"
-              when 'bold|italic'
+              when type == 'bold|italic'
                 "<emph type=\"10\">#{inner_text}</emph>"
-              when 'bold|superscript'
+              when type == 'bold|superscript'
                 "<emph type=\"1\"><emph type=\"7\">#{inner_text}</emph></emph>"
-              when 'italic|underline'
+              when type == 'italic|underline'
                 "<emph type=\"2\"><emph type=\"4\">#{inner_text}</emph></emph>"
-              when 'italic|smallcaps'
+              when type == 'italic|smallcaps'
                 "<emph type=\"2\"><emph type=\"6\">#{inner_text}</emph></emph>"
-              when 'bold'
+              when type == 'bold'
                 "<emph type=\"1\">#{inner_text}</emph>"
               when /italic/.match(type)
                 "<emph type=\"2\">#{inner_text}</emph>"
-              when 'underline'
+              when type == 'underline'
                 "<emph type=\"4\">#{inner_text}</emph>"
               when /smallcaps/.match(type)
                 "<emph type=\"6\">#{inner_text}</emph>"
               when /superscript/.match(type)
                 "<emph type=\"7\">#{inner_text}</emph>"
-              when 'subscript'
+              when type == 'subscript'
                 "<emph type=\"8\">#{inner_text}</emph>"
-              when 'allcaps'
+              when type == 'allcaps'
                 inner_text.to_upper
               else
                 inner_text
               end
-    node.name = 'p'
-    node.set_attribute('align', (front_matter ? 'center' : 'left'))
-    node.remove_attribute 'type'
-    node.inner_html = replace
+    node.add_next_sibling(replace)
+    node.remove
   end
 end
